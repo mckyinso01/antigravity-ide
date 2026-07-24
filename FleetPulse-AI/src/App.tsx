c3d86a34168716796f6f5c199ab0c56cc864d568
Mckinsyo01 <mckinsyo01@gmail.com>
2026-07-24 19:53:15 +0800

    refactor: remove FleetPulse-AI project as requested by user

--- FleetPulse-AI/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { WazeCompactCommercialSuite } from './components/WazeCompactCommercialSuite';
-
-export default function App() {
-  return <WazeCompactCommercialSuite />;
-}
